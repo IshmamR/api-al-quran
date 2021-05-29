3fc421fe9fe7b6bf1d76e4d3d1004c7bc8407dce
IshmamR <ishmam785@gmail.com>
2021-05-29 23:15:24 +0600

    sqlite db created

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+app.use(cors());
 
 const path = require('path');
 const fs = require('fs');
-// const data = require('./data/imgData.json');
 
-app.use(cors());
+const db = require('./sqliteModel.js');
 
 app.get('/', (req, res) => {
-	console.log('here');
 	res.status(200).sendFile(path.join(__dirname, './index.html'));
 })
 app.get('/home', (req, res) => {
@@ -19,36 +18,89 @@ app.get('/home', (req, res) => {
 app.get('/api/imgs', (req, res) => {
 	fs.readFile(path.join(__dirname, '/data/imgData.json'), 'utf-8', (err, data) => {
 		if(!err) res.status(200).json(JSON.parse(data));
-		else {
-			res.status(404).json({error: '404 not found'})
-			throw err;
-		}
+		else { res.status(404).json({error: '404 not found'}); throw err; }
 	})
 })
 
-app.get('/api/quran-full', (req, res) => {
-	fs.readFile(path.join(__dirname, '/data/Quran-min.json'), 'utf-8', (err, data) => {
-		if(!err) res.status(200).json(JSON.parse(data));
-		else {
-			res.status(404).json({error: '404 not found'})
-			throw err;
-		}
-	})
-})
-
-app.get('/page', (req, res) => {
+app.get('/api/page', (req, res) => {
 	const pageNo = req.query.p;
-	console.log(pageNo); // outputs undefined if nor in query string
+	// console.log(pageNo); // outputs undefined if not in query string
 	if (pageNo !== undefined && pageNo !== 'undefined' && pageNo > 0 && pageNo <= 604) {
 		res.status(200).sendFile(path.join(__dirname, `/quran-images/${pageNo}.png`));
-	} 
-	else {
+	} else {
 		res.status(404).send('<h1>404, Page not found </h1>');
 	}
 })
 
+
+app.get('/api/v1/quran-full', (req, res) => {
+	fs.readFile(path.join(__dirname, '/data/Quran-min.json'), 'utf-8', (err, data) => {
+		if(!err) res.status(200).json(JSON.parse(data));
+		else { res.status(404).json({error: '404 not found'}); throw err; }
+	})
+})
+
+app.get('/api/v2/quran-full', (req, res) => {
+	var ayahs;
+	fs.readFile(path.join(__dirname, '/data/ayahs.json'), 'utf-8', (err, data) => {
+		if(err) { res.status(500).json({error: 'Internal server error'}); throw err; }
+		else {
+			ayahs = JSON.parse(data).ayahs;
+			db.all("SELECT * FROM Surahs ORDER BY number", [], (err, surahs) => {
+				if (err) { res.status(500).json({error: 'Internal server error'}); throw err; }
+				else {
+					surahs.forEach((surah, i) => {
+						let arr = ayahs.filter(ayah => ayah.surah === i+1);
+						surah.ayahs = arr;
+					});
+					res.status(200).json( { surahs: surahs } );
+				}		
+			})
+		}
+	})
+})
+
+app.get('/api/v3/quran-full', (req, res) => {
+	db.all(`SELECT * FROM Ayahs ORDER BY number`, [], (err, ayahs) => {
+		if (err) { res.status(500).json({error: 'Internal server error'}); throw err; }
+		else {
+			db.all("SELECT * FROM Surahs ORDER BY number", [], (err, surahs) => {
+				if (err) return console.error(err.message);
+				else {
+					surahs.forEach((surah, i) => {
+						let arr = ayahs.filter(ayah => ayah.surah === i+1)
+						surah.ayahs = arr;
+					});
+					res.status(200).json( {surahs: surahs} );
+				}
+			})
+		}
+	})
+})
+
+
+app.get("/api/surahs", (req, res) => {
+	const sql = "SELECT * FROM Surahs ORDER BY number";
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		res.status(200).json( { surahs: rows } );
+	});
+});
+app.get("/api/ayahs", (req, res) => {
+	const sql = "SELECT * FROM Ayahs ORDER BY number";
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		res.status(200).json( { ayahs: rows } );
+	});
+});
+
+
 // 404 PAGE
-app.use((req, res,next) => {
+app.use((req, res, next) => {
    res.status(404).send('<h1>404, Page not found </h1>');
 });
 
---
+++ sqliteModel.js
@@ -0,0 +1,46 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require('path');
+
+const db_name = path.join(__dirname, "db", "quran.db");
+const db = new sqlite3.Database(db_name, err => {
+	if (err) return console.error(err.message);
+	// console.log("Successful connection to the database 'quran.db'");
+});
+
+const sql_create_surahs = `CREATE TABLE IF NOT EXISTS Surahs (
+	Surah_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	number INTEGER not NULL,
+	name STRING utf8, 
+	englishName STRING utf8, 
+	englishNameTranslation STRING utf8, 
+	revelationType STRING utf8, 
+	banglaName STRING utf8 
+);`;
+db.run(sql_create_surahs, err => {
+	if (err) return console.error(err.message);
+	// console.log("Successful creation of the 'Surahs' table");
+});
+
+const sql_create_ayahs = `CREATE TABLE IF NOT EXISTS Ayahs (
+	Ayah_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	number INTEGER not NULL,
+	numberInSurah INTEGER,
+	surah INTEGER,
+	audio STRING utf8,
+	text STRING utf8,
+	juz INTEGER,
+	manzil INTEGER,
+	page INTEGER,
+	ruku INTEGER,
+	hizbQuarter INTEGER,
+	sajda BOOLEAN,
+	pngImage STRING utf8,
+	textInEnglish STRING utf8,
+	textInBangla STRING utf8
+);`;
+db.run(sql_create_ayahs, err => {
+	if (err) return console.error(err.message);
+	// console.log("Successful creation of the 'Ayahs' table");
+});
+
+module.exports = db;
